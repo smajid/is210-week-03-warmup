f39d2bb665a726eee8ac136558ec93887658245b
Saulat Majid <saulat.majid@spsmail.cuny.edu>
2015-02-13 03:31:08 +0000

    a lengthy docstring.

--- task_09.py
+++ task_09.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
 
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
+"""
+
+
