44e517471acb2176cde22702a4744cef5bada698
Saulat Majid <saulat.majid@spsmail.cuny.edu>
2015-02-13 04:41:23 +0000

    importing decimals and fractions.

---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""File # twelve has been created and it it contains import statements"""
+import fractions
+import decimal
+
+INTVAL = 1
+FLOATVAL = 0.1
+print INTVAL
+print FLOATVAL
+
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
+print DECVAL
+print FRACVAL
