a7d45e33c53b4e076512857b8562d791b850778e
Saulat Majid <saulat.majid@spsmail.cuny.edu>
2015-02-13 05:23:51 +0000

    worked with boolean.

---
+++ task_14.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""file number fourteen created."""
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+print IS_TRUE
+
+print IS_FALSE
+
+print IS_NONE
+
+INTEGER_EQUIV = ((IS_TRUE == 1) and (IS_FALSE == 0))
+
+print INTEGER_EQUIV
